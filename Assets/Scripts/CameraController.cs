21145c6d517e95cad15b11ee06085e79c2e28fb8
Bruno <brunobannunciato@gmail.com>
2022-02-09 21:12:28 -0300

    :sparkles: added a limit to camera follow character

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour
 {
+
+    [SerializeField] Tilemap _map;
+    Vector3 minBoundMap;
+    Vector3 maxBoundMap;
+
     Transform target;
     void Start()
     {
+        minBoundMap = _map.localBounds.min;
+        maxBoundMap = _map.localBounds.max;
+
         target = PlayerController.instance.transform;
         Debug.Log(target);
     }
@@ -14,6 +23,7 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        //transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, minBoundMap.x, maxBoundMap.x), Mathf.Clamp(target.position.y, minBoundMap.y, maxBoundMap.y), transform.position.z);
     }
 }
